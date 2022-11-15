8b00d46ecd5ce3dd0a8d0425778675ea3014ec56
Cody <alexhill318@yahoo.com>
2022-11-14 19:05:19 -0600

    Nav Change/Mobile Functionality

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 const faders = document.querySelectorAll('.fade-in');
 
+let header = document.querySelector('.header');
+let navbar = document.querySelector('.navbar');
+let bars = document.getElementById('menu-bar');
+let appoint = document.querySelector('.btn')
+
+window.onscroll = () => {
+    if (window.scrollY > 100) {
+        header.classList.add('active');
+    } else {
+        header.classList.remove('active');
+    }
+};
+
+bars.addEventListener('click', () => {
+    bars.style.color = 'black';
+})
+
+appoint.addEventListener('click', () => {
+    appoint.style.backgroundColor = 'black';
+    setTimeout(() => {
+        appoint.style.backgroundColor = 'rgb(13, 73, 122)'
+    }, 2000);
+})
+
+
 const appearOptions = {
     // once the entire section/area is visible it will fade in
     threshold: 1,
